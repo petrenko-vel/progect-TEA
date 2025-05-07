e6c06b26da060a1f79c2719f89e638349e0a92d0
Valeria Yatsuk <proslera@yandex.ru>
2025-05-07 22:20:20 +0300

    creatted global.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,12 @@
-import { Head } from "minista"
-
+export const metadata = {
+  title: 'Home',
+}
 
 export default function () {
   return (
     <>
-      <Head>
-        <title>Hello!</title>
-      </Head>
-      <h1>Hello!</h1>
+      <h1>Главная</h1>
+
     </>
   )
 }
---
+++ src/pages/movies.jsx
@@ -0,0 +1,14 @@
+export const metadata = {
+    title: 'Movies & Shows',
+  }
+  
+
+export default function () {
+    return (
+      <>
+        <h1>Фильмы</h1>
+  
+      </>
+    )
+  }
+  
